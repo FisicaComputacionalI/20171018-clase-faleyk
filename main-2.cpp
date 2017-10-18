ce28155fa27c7670c5f862b691e90727827ac18f
Fab <fab.cyl@gmail.com>
2017-10-18 13:45:04 -0500

    Update main-2.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,4 +1,4 @@
-//programa que suma los números pares entre el 1 y el 20
+//programa que suma los números pares entre el 1 y el 20, incluyendo el 20
 //Autor: Fabiola Canete
 //18 octubre 2017
 #include <iostream>
@@ -9,8 +9,8 @@ int main()
 {
     int sum=0, count =1;
 
-    while (count < 21) {
-        if ((count%2)<1){
+    while (count < 21) { //podemos escribir también (count<=20)
+        if ((count%2)<1){ //podemos escribir también (count%2)==0
             sum=sum+count;
         }
         count++;
